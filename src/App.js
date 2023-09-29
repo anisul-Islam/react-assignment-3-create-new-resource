babc294b51ada98866adaeeebe074f8d1fb2f135
rahat-md <rahat013166@gmail.com>
2023-09-29 12:22:19 +0600

    complete assignment 3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -21,12 +22,16 @@ const App = () => {
   const [todos, setTodos] = useState(todosData);
 
   // get the newTodo from NewTodo.js here inside this function
-  const handleAddTodo = () => {};
+  const handleAddTodo = (addTodo) => {
+    setTodos((prevTodo) => {
+      return [...prevTodo, addTodo];
+    });
+  };
 
   return (
     <div>
-      <NewTodo />
-      <Todos />
+      <NewTodo onHandleAddTodo={handleAddTodo} />
+      <Todos todos={todos} />
     </div>
   );
 };
